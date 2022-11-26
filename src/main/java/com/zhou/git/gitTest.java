835a5024cc80fca34ab312a49376e627b1eded55
zhou <1678248957@qq.com>
2022-11-26 11:00:15 +0800

    push commit

--- src/main/java/com/zhou/git/gitTest.java
+++ src/main/java/com/zhou/git/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
         System.out.println("hello,git!3-----hot-fix");
         System.out.println("master test");
         System.out.println("hot-fix");
+        System.out.println("push-commit");
     }
 }
